3a264437003d0297f7e07c3994d970d388cb8252
CUR53 <114309130+CUR-53@users.noreply.github.com>
2023-05-16 13:36:00 +0200

    update slides with content

--- scripts/info_slider.js
+++ scripts/info_slider.js
@@ -7,21 +7,21 @@ function showSlide(n) {
   slides[currentSlide].classList.add('active');
   slides[currentSlide].style.transform = 'translateX(-100%)';
   setTimeout(function () {
-    slides[currentSlide].style.transition = 'transform 0.6s';
+    slides[currentSlide].style.transition = 'transform 1s';
     slides[currentSlide].style.transform = 'translateX(0)';
   }, 100);
 }
 
 function nextSlide() {
-  slides[currentSlide].style.transition = 'transform 0.6s';
+  slides[currentSlide].style.transition = 'transform 1s';
   slides[currentSlide].style.transform = 'translateX(100%)';
   setTimeout(function () {
     slides[currentSlide].style.transition = '';
     slides[currentSlide].style.transform = 'translateX(-100%)';
-    setTimeout(showSlide, 2333, currentSlide + 1);
-  }, 500);
+    setTimeout(showSlide, 3333, currentSlide + 1);
+  }, 3333);
 }
 
-setInterval(nextSlide, 7000);
+setInterval(nextSlide, 15000);
 
 showSlide(currentSlide);
